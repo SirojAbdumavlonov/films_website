d6fd6f6bf8f3adb550f68347060abb93204e25fb
Sirojiddin <siroj.abdumavlonov369@gmail.com>
2023-06-03 22:39:15 +0500

    ready

--- src/main/java/com/example/backend_prj/entity/User.java
+++ src/main/java/com/example/backend_prj/entity/User.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
     private String fullname;
